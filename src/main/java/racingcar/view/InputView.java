a9974afa2db2d046535335afd4514f826809f7a4
soheeGit <63wlsthgml@gmail.com>
2025-10-23 19:56:07 +0900

    feat: 입력 기능 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,13 +1,34 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputView {
+    private static final String CAR_NAMES_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String ATTEMPT_COUNT_INPUT_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    private static final String COMMA_DELIMITER = ",";
+    
     public List<String> readCarNames() {
-        return null;
+        System.out.println(CAR_NAMES_INPUT_MESSAGE);
+        String input = Console.readLine();
+        return parseCarNames(input);
+    }
+    
+    private List<String> parseCarNames(String input) {
+        return Arrays.stream(input.split(COMMA_DELIMITER))
+                .map(String::trim)
+                .collect(Collectors.toList());
     }
     
     public int readAttemptCount() {
-        return 0;
+        System.out.println(ATTEMPT_COUNT_INPUT_MESSAGE);
+        String input = Console.readLine();
+        return parseAttemptCount(input);
+    }
+    
+    private int parseAttemptCount(String input) {
+        return Integer.parseInt(input);
     }
 }
