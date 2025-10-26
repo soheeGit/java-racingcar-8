b04b8021a1ea908f0b7ce253d83d77a725042a60
soheeGit <63wlsthgml@gmail.com>
2025-10-26 16:29:07 +0900

    feat: 출력 기능

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,16 +1,24 @@
 package racingcar.view;
 
+import racingcar.domain.Car;
 import racingcar.domain.Cars;
 import racingcar.domain.Winners;
 
 public class OutputView {
 
     public void printResultHeader() {
+        System.out.println();
+        System.out.println("실행 결과");
     }
     
     public void printRoundResult(Cars cars) {
+        for (Car car : cars.getCars()) {
+            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
+        }
+        System.out.println();
     }
     
     public void printWinners(Winners winners) {
+        System.out.println("최종 우승자 : " + winners);
     }
 }
