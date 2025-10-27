3df3d08706d3b9ecdfab2bfe1ade7ef4ca0a0a0c
soheeGit <63wlsthgml@gmail.com>
2025-10-27 19:27:45 +0900

    remove: 주석 삭제

--- src/main/java/racingcar/exception/ErrorMessage.java
+++ src/main/java/racingcar/exception/ErrorMessage.java
@@ -1,16 +1,13 @@
 package racingcar.exception;
 
 public class ErrorMessage {
-    // Name 검증
     public static final String EMPTY_NAME = "자동차 이름은 빈 문자열일 수 없습니다.";
     public static final String INVALID_NAME_LENGTH = "자동차 이름은 5자를 초과할 수 없습니다.";
     public static final String DUPLICATE_NAME = "중복된 자동차 이름이 존재합니다.";
 
-    // 시도 횟수 검증
     public static final String INVALID_ATTEMPT_COUNT = "시도 횟수는 양수여야 합니다.";
     public static final String INVALID_NUMBER_FORMAT = "시도 횟수는 숫자여야 합니다.";
 
-    // 랜덤값 검증
     public static final String INVALID_RANDOM_NUMBER = "무작위 값은 0~9 사이여야 합니다.";
 
     private ErrorMessage() {
