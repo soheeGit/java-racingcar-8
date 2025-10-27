d260c216ade22064dd891fd8f733033bfee1a2f8
soheeGit <63wlsthgml@gmail.com>
2025-10-27 19:46:50 +0900

    refector: 상수 이름 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -11,7 +11,7 @@ public class InputView {
     private static final String CAR_NAMES_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String ATTEMPT_COUNT_INPUT_MESSAGE = "시도할 횟수는 몇 회인가요?";
     private static final String COMMA_DELIMITER = ",";
-    private static final int MIN_NUMBER = 0;
+    private static final int MIN_ATTEMPT_COUNT = 1;
     
     public List<String> readCarNames() {
         System.out.println(CAR_NAMES_INPUT_MESSAGE);
@@ -35,7 +35,7 @@ public class InputView {
     private void validateAttemptCount(String input) {
         try {
             int number = Integer.parseInt(input);
-            if(number <= MIN_NUMBER) {
+            if(number < MIN_ATTEMPT_COUNT) {
                 throw new IllegalArgumentException(ErrorMessage.INVALID_ATTEMPT_COUNT);
             }
         } catch (NumberFormatException e) {
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -8,7 +8,7 @@ public class OutputView {
 
     private static final String RESULT_HEADER_MESSAGE = "실행 결과";
     private static final String WINNER_PREFIX = "최종 우승자";
-    private static final String NAME_POSITION_DELIMITER = " : ";
+    private static final String LABEL_CONTENT_DELIMITER = " : ";
 
     public void printResultHeader() {
         System.out.println();
@@ -17,12 +17,12 @@ public class OutputView {
     
     public void printRoundResult(Cars cars) {
         for (Car car : cars.getCars()) {
-            System.out.println(car.getName() + NAME_POSITION_DELIMITER + car.getPositionDisplay());
+            System.out.println(car.getName() + LABEL_CONTENT_DELIMITER + car.getPositionDisplay());
         }
         System.out.println();
     }
     
     public void printWinners(Winners winners) {
-        System.out.println(WINNER_PREFIX + NAME_POSITION_DELIMITER + winners);
+        System.out.println(WINNER_PREFIX + LABEL_CONTENT_DELIMITER + winners);
     }
 }
