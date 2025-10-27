bac30853a95e9fcf94342962451c2d93d5f1cad2
soheeGit <63wlsthgml@gmail.com>
2025-10-27 19:26:35 +0900

    remove: 불필요 메서드 삭제

--- src/main/java/racingcar/domain/Name.java
+++ src/main/java/racingcar/domain/Name.java
@@ -6,12 +6,4 @@ public record Name(String value) {
     public Name {
         Validator.validateName(value);
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Name name = (Name) o;
-        return value.equals(name.value);
-    }
 }
